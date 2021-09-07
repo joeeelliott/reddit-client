3e6103a962f0f09dfafb48147e45ad86b48feffe
Joe <joeelliott310@gmail.com>
2021-09-07 10:08:10 +0100

    create loading modal, still requires connection to articleSlice to only display when neccessary

--- src/app/App.js
+++ src/app/App.js
@@ -5,13 +5,27 @@ import Article from '../components/Article';
 import Footer from '../components/Footer'; 
 
 function App() {
+  // <script src="jquery-2.1.4.js"></script>
+
   return (
     <div data-test="App">
+      <div className="App_loading-wrapper">
+        <span class="loader"><span class="loader-inner"></span></span>
+      </div>
+      
       <Header />
       <div className="article_container">
         <Article />
       </div>
       <Footer />
+
+      {/* <script>
+        $(window).on('load', function() {
+          $('.loader-wrapper').fadeOut('slow')
+        });
+      </script> */}  
+      
+      {/* {need to find a way to load this in on window load - maybe in article slice will pop as no display for now but thats not currently pushed and merged to master yet so will do that later} */}
     </div>
   );
 }
